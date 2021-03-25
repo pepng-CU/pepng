e960705704a8b7e73310188a2d592fc9f186788f
alexandre-lavoie <alexandre.lavoie00@gmail.com>
2021-03-24 22:13:16 -0400

    Random recenter.

--- src/component/transformer.cpp
+++ src/component/transformer.cpp
@@ -64,7 +64,7 @@ void Transformer::update(std::shared_ptr<WithComponents> parent) {
     }
 
     if(input->getButtonDown("recenter")) {
-        transform->position = glm::vec3(0.0f);
+        transform->position = glm::vec3(rand() % 100 - 50, 0.0f, rand() % 100 - 50);
         transform->setRotation(glm::vec3(0.0f));
         transform->scale = glm::vec3(1.0f);
         transform->shear = glm::vec3(0.0f);
